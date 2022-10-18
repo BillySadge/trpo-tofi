3c7cad83632813f070058d3c255530e4129f13a2
V14dik <cherenkevich.vlad@gmail.com>
2022-10-18 22:45:51 +0300

    mark up book screen

--- library/frontend/src/screens/BookScreen.js
+++ library/frontend/src/screens/BookScreen.js
@@ -1,11 +1,77 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import books from '../books'
 
-function BookScreen() {
-  return (
-    <div>
-      Book
-    </div>
-  )
+function BookScreen({...match }) {
+    const {id} = useParams()
+    const book = books.find((book) => book._id === id)
+    return (
+        <div>
+            <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+            <Row>
+
+                <Col md={6}>
+                    <Image src={book.image} alt={book.name} fluid />
+                </Col>
+
+                <Col md={3}>
+                    <ListGroup variant="flush">
+
+                        <ListGroup.Item>
+                            <h3>{book.name}</h3>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Rating value={book.rating} text={`${book.numReviews} reviews`} color={'#f8e825'} />
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Price: ${book.price}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            Description: ${book.description}
+                        </ListGroup.Item>
+
+                    </ListGroup>
+                </Col>
+
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${book.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        <strong>{book.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <div class="d-grid gap-2">
+                                    <Button class="btn btn-primary" disabled={book.countInStock === 0} type="button">ADD TO CART</Button>
+                                </div>
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default BookScreen
