c1fe1d47bbdac6520659e2b46962f1206169b18d
BillySadge <billywillneverreturn@gmail.com>
2022-10-24 21:09:06 +0300

    register user | handle exception

--- library/backend/base/urls.py
+++ library/backend/base/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
+    # path('', views.getRoutes, name="routes"),
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name="register"),
     path('users/profile/', views.getUserProfile, name="user-profile"),
     path('users/', views.getUsers, name="users"),
 
--- library/backend/base/views.py
+++ library/backend/base/views.py
@@ -10,8 +10,8 @@ from .serializer import BookSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-
-
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -28,20 +28,39 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/books/',
-        '/api/books/create/',
-        '/api/books/upload/',
-        '/api/books/<id>/reviews/',
-        '/api/books/top/',
-        '/api/books/<id>/',
-        '/api/books/delete/<id>/',
-        '/api/books/update/<id>/',
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+
+    try:
+        user = User.objects.create( 
+            first_name = data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except: 
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET'])
+# def getRoutes(request):
+#     routes = [
+#         '/api/books/',
+#         '/api/books/create/',
+#         '/api/books/upload/',
+#         '/api/books/<id>/reviews/',
+#         '/api/books/top/',
+#         '/api/books/<id>/',
+#         '/api/books/delete/<id>/',
+#         '/api/books/update/<id>/',
         
-    ]
-    return Response(routes)
+#     ]
+#     return Response(routes)
 
 
 
