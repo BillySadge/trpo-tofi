7084d38141b583ffb0be2efe017d33dff19bdf40
BillySadge <billywillneverreturn@gmail.com>
2022-10-24 20:50:06 +0300

    add protected routes

--- library/backend/base/urls.py
+++ library/backend/base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('users/', views.getUsers, name="users"),
 
     path('books/', views.getBooks, name="books"),
     path('books/<str:pk>', views.getBook, name="book"),
--- library/backend/base/views.py
+++ library/backend/base/views.py
@@ -1,16 +1,14 @@
-import imp
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .books import books
+from django.contrib.auth.models import User
 from .models import Book
-from .serializer import BookSerializer
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializer import BookSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 
@@ -48,11 +46,20 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+ 
 
 
 @api_view(['GET'])
@@ -60,6 +67,7 @@ def getBooks(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
+
 
 
 @api_view(['GET'])
